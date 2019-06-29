6138fb961059eac1b6fde831c0a2ab9275d7058b
Christian Pérez Villanueva <christian.perez34@upr.edu>
2019-06-29 10:21:38 -0400

    Initial commit

---
+++ src/Fetch.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from "svelte";
+  import axios from "axios";
+  // export the variable to make a prop
+  export let url = "https://jsonplaceholder.typicode.com/todos";
+  let data = [];
+  onMount(async function() {
+    axios.get(url).then(response => {
+      data = response.data;
+    });
+  });
+</script>
+
+{@debug data}
+<ul>
+  {#each data as link}
+    <li>
+      <a href={link.url}>{link.title}</a>
+    </li>
+  {/each}
+</ul>
