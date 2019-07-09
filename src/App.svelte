5062996df2185f37ea3d294d46b090ac8f5c543e
Christian Pérez Villanueva <christian.perez34@upr.edu>
2019-07-09 19:21:25 -0400

    Added basic navbar with placeholder links

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,39 @@
 </script>
 
 <style>
-  h1 {
-    color: purple;
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+  }
+
+  li {
+    float: left;
+  }
+
+  li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+
+  li a:hover:not(.active) {
+    background-color: #777;
+  }
+
+  .active {
+    background-color: #BBB;
   }
 </style>
 
-<!-- <h1>Hello {name}!</h1> -->
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#login">Login</a></li>
+  <li><a href="#signup">Sign-Up</a></li>
+</ul>
 
 <Fetch />
--- src/Fetch.svelte
+++ src/Fetch.svelte
@@ -11,7 +11,7 @@
   });
 </script>
 
-{@debug data}
+<!-- {@debug data} -->
 <ul>
   {#each data as link}
     <li>
